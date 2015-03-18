7a513ed20953d829c0c80c950c571fd3fcd4bf94
Edwin Sutanudjaja <edwinhs@cartesius.surfsara.nl>
2015-03-18 18:14:53 +0100

    Initial commit.

---
+++ multiCore.py
@@ -0,0 +1,136 @@
+# The script is inspired by Menno Straatsma.
+# 12 March 2014: Edwin started modifying some lines.
+
+import string
+import os 
+import glob
+import numpy as np
+
+def filesPerCore(fileList, ncores):
+	'''list the number of files mper core resulting in an similar total 
+	   file size'''
+	coreFileSize = range(-ncores, 0, 1)
+	outFileList = []
+	for i in range(ncores):
+		outFileList.append([])
+	for areaFile in fileList:
+		areaSize = os.path.getsize(areaFile)
+		minCoreIndex = coreFileSize.index(min(coreFileSize))
+		coreFileSize[minCoreIndex] += areaSize
+		outFileList[minCoreIndex].append(areaFile)
+	print('fileSizes: ')
+	print(coreFileSize)
+	return outFileList
+
+########################################################################
+
+# clone map files that will be used
+fileList = glob.glob('/projects/dfguu/data/hydroworld/others/05ArcMinCloneMaps/new_masks_from_top/clone_M*.map')
+
+# typical ini files
+dummy_ini_filename = '/home/edwinhs/github/edwinkost/PCR-GLOBWB/config/runs_5min_nov_2014/test_at_cartesius/setup_dummy_natural_re-run.ini'
+dummy =  open(dummy_ini_filename,'r').read()
+
+# output directories for model results
+outputDirRoot  = '/projects/wtrcycle/users/edwinhs/05min_runs_28_november_2014/multi_cores_natural_1960_to_2010/%s/'
+
+#-output directory for ini files and batch files 
+batchFilePath  = '/home/edwinhs/jobs/multi_core_jobs_28_nov_2014/natural/batch_files/'
+iniFilesPath   = '/home/edwinhs/jobs/multi_core_jobs_28_nov_2014/natural/ini_files/'
+
+# master job file (that will be submitted as a cartesius job)
+masterBatchFile = 'natural_1960_to_2010.sh'
+
+# main python file/script to run the model
+modelScriptFolderInAbsolutePath = "/home/edwinhs/github/edwinkost/PCR-GLOBWB/model/"
+modelFileName  = modelScriptFolderInAbsolutePath+'deterministic_runner.py'
+
+# to be writen in the ini files: file names for clone, landMask and ldd:
+cloneMapPath   = '/projects/dfguu/data/hydroworld/others/05ArcMinCloneMaps/new_masks_from_top/clone_%s.map'
+landmaskPath   = '/projects/dfguu/data/hydroworld/others/05ArcMinCloneMaps/new_masks_from_top/mask_%s.map'  
+
+# number of cores that will be used
+ncores = 53 # from 53 maks
+
+
+# walltime, number and type of node:
+wall_time      = '119:50:00' # '50:00'            # '119:50:00'
+number_of_node =  1          # 1                  # 1           #
+type_of_node   = 'fat'       # 'short'            # 'fat'       # options are fat, normal and short
+
+# preparing directories:
+try:
+	os.makedirs(iniFilesPath)
+except:
+	pass
+try:
+	os.makedirs(batchFilePath)
+except:
+	pass
+os.system('rm -r '+iniFilesPath+"/*")
+os.system('rm -r '+batchFilePath+"/*")
+
+iniFileBase    = 'setup_%s.ini'				   # % (area)  
+batchFileBase  = 'batch_%scores_core%s.txt'    # % (ncores, core_i)
+
+########################################################################
+
+############ main ######################################################
+
+cores= range(1,ncores+1)
+
+#- create master batch file and individual batch files for each core
+outMaster = open(os.path.join(batchFilePath,masterBatchFile), 'w')
+# - write header for the job file
+masterLine = '#!/bin/bash\n'
+outMaster.write(masterLine)  
+masterLine = '#SBATCH -N %i\n' % (number_of_node)
+outMaster.write(masterLine)  
+masterLine = '#SBATCH -t %s\n' % (wall_time)
+outMaster.write(masterLine)  
+masterLine = '#SBATCH -p %s\n' % (type_of_node)
+outMaster.write(masterLine)  
+for coreIdx in cores:
+	#-fill master batch file with bash command that starts a single core
+	masterLine = 'bash %s & \n' % os.path.join(batchFileBase % (ncores, coreIdx))
+	outMaster.write(masterLine)
+	#-create a single batch file for each core
+	batchFileName = os.path.join(batchFilePath, batchFileBase % (ncores, coreIdx))
+	outFileCore = open(batchFileName, 'w')
+	outFileCore.close()
+outMaster.write('wait \n')
+outMaster.close()
+
+#- create a list of files to be processed by each core and write files
+filesPerNcores = filesPerCore(fileList, ncores)
+
+for coreIdx in cores:
+	for cloneFile in filesPerNcores[cores.index(coreIdx)]:
+		
+		area = cloneFile[-7:-4]
+		
+		#- replace strings in dummy file
+		d2 = string.replace(dummy, 'OUTPUTDIRECTORY', outputDirRoot%area)
+		d3 = string.replace(d2,    'CLONEMAPFILES',    cloneMapPath%area)
+		d4 = string.replace(d3,    'LANDMASKFILES',    landmaskPath%area)
+		
+		last = d4
+		
+		#- write ini file to disc
+		iniOutputName = os.path.join(iniFilesPath, iniFileBase % area)
+		outFile = file(iniOutputName, 'w')
+		outFile.write(last)
+		outFile.close()
+
+		#- list .ini file for "area" in the batch file with least lines
+		batchFileNameCore = os.path.join(batchFilePath, batchFileBase%(ncores, coreIdx))
+		batchOutFile = file(batchFileNameCore, 'a')
+		
+		# write python command line
+		#~ batchLine = 'numactl --localalloc --physcpubind=%s python /home/beek0120/PCR-GLOBWB/scripts/twoLayersNewSoilParams/PCR-GLOBWB_TwoLayers.py %s\n' % (coreIdx, iniOutputName)
+		#~ batchLine = 'numactl --localalloc --physcpubind=%s python %s %s\n' % (coreIdx, modelFileName, iniOutputName)
+		batchLine = 'python %s %s\n' % (modelFileName, iniOutputName)
+		batchOutFile.write(batchLine)
+
+		batchOutFile.close()
+		print 'creating %s  and line added to %s' %(iniOutputName, batchFileNameCore)
